ad2653cf1896301324ed949aa1c5b844a5d4c64a
Tim DuBois <genesis@neophilus.net>
2013-04-02 14:54:37 +1100

    Total revamp of key bindings. Added image of current layout

--- xmonad.hs
+++ xmonad.hs
@@ -10,6 +10,7 @@ import XMonad.Layout.ResizableTile
 import XMonad.Layout.Circle
 import XMonad.Layout.Spiral
 import qualified XMonad.StackSet as W
+import qualified Data.Map as M
 
 import XMonad.Actions.GridSelect
 -- WINDOW RULES
@@ -64,15 +65,15 @@ myLayout = defaultLayouts
 -- Declare workspaces and rules for applications
 
 myWorkspaces = clickable $
-                ["^i(/home/genesis/.xmonad/icons/mail.xbm) mail"
-                ,"^i(/home/genesis/.xmonad/icons/globe.xbm) web"
-                ,"^i(/home/genesis/.xmonad/icons/shell.xbm) shell"
-                ,"^i(/home/genesis/.xmonad/icons/monitor.xbm) work"
-                ,"^i(/home/genesis/.xmonad/icons/calc.xbm) code"
-                ,"^i(/home/genesis/.xmonad/icons/text.xbm) latex"
-                ,"^i(/home/genesis/.xmonad/icons/picture.xbm) image"
-                ,"^i(/home/genesis/.xmonad/icons/movie.xbm) video"
-                ,"^i(/home/genesis/.xmonad/icons/doc.xbm) docs"]
+                ["^i(/home/genesis/.xmonad/icons/mail.xbm) alpha"
+                ,"^i(/home/genesis/.xmonad/icons/globe.xbm) beta"
+                ,"^i(/home/genesis/.xmonad/icons/shell.xbm) gamma"
+                ,"^i(/home/genesis/.xmonad/icons/monitor.xbm) delta"
+                ,"^i(/home/genesis/.xmonad/icons/calc.xbm) epsilon"
+                ,"^i(/home/genesis/.xmonad/icons/text.xbm) stigma"
+                ,"^i(/home/genesis/.xmonad/icons/picture.xbm) zeta"
+                ,"^i(/home/genesis/.xmonad/icons/movie.xbm) eta"
+                ,"^i(/home/genesis/.xmonad/icons/doc.xbm) theta"]
         where clickable l     = [ "^ca(1,xdotool key super+" ++ show (n) ++ ")" ++ ws ++ "^ca()" |
                                     (i,ws) <- zip [1..] l,
                                     let n = i ]
@@ -135,47 +136,22 @@ main = do
                 , logHook               = myLogHook dzenLeftBar -- >> fadeInactiveLogHook 0xdddddddd
                 }
                 `additionalKeys`
-                [((mod4Mask .|. shiftMask       , xK_b), spawn "chromium")
-                ,((mod4Mask .|. shiftMask       , xK_t), spawn "urxvt -e tmux")
+                [((mod4Mask .|. shiftMask       , xK_x), kill)
 --              ,((mod4Mask .|. shiftMask       , xK_r), spawn "dmenu_run -nb '#000000' -nf '#404040' -sb '#000000' -sf '#FFFFFF' -fn '-*-lime-*-*-*-*-*-*-*-*-*-*-*-*'")
                 ,((mod4Mask .|. shiftMask       , xK_r), spawn "dmenu_run")
                 ,((mod4Mask                     , xK_q), spawn "killall dzen2; killall conky; cd ~/.xmonad; ghc -threaded xmonad.hs; mv xmonad xmonad-x86_64-linux; xmonad --restart" )
-                ,((mod4Mask .|. shiftMask       , xK_x), kill)
-                ,((mod4Mask .|. shiftMask       , xK_c), return())
-                ,((mod4Mask                     , xK_p), moveTo Prev NonEmptyWS)
-                ,((mod4Mask                     , xK_n), moveTo Next NonEmptyWS)
-                ,((mod4Mask                     , xK_c), moveTo Next EmptyWS)
-                ,((mod4Mask .|. shiftMask       , xK_l), sendMessage MirrorShrink)
-                ,((mod4Mask .|. shiftMask       , xK_h), sendMessage MirrorExpand)
---              ,((mod4Mask                     , xK_v), screenWorkspace sc >>= flip whenJust (windows . f))
-                ,((mod4Mask                     , xK_a), withFocused (keysMoveWindow (-20,0)))
-                ,((mod4Mask                     , xK_comma), withFocused (keysMoveWindow (0,-20)))
-                ,((mod4Mask                     , xK_o), withFocused (keysMoveWindow (0,20)))
-                ,((mod4Mask                     , xK_e), withFocused (keysMoveWindow (20,0)))
-                ,((mod4Mask .|. shiftMask       , xK_a), withFocused (keysResizeWindow (-20,0) (0,0)))
-                ,((mod4Mask .|. shiftMask       , xK_comma), withFocused (keysResizeWindow (0,-20) (0,0)))
-                ,((mod4Mask .|. shiftMask       , xK_o), withFocused (keysResizeWindow (0,20) (0,0)))
-                ,((mod4Mask .|. shiftMask       , xK_e), withFocused (keysResizeWindow (20,0) (0,0)))
-                ,((mod4Mask                     , xK_F1), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_music.sh")
-                ,((mod4Mask                     , xK_F2), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_vol.sh")
-                ,((mod4Mask                     , xK_F3), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_network.sh")
-                ,((mod4Mask                     , xK_F4), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_battery.sh")
-                ,((mod4Mask                     , xK_F5), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_hardware.sh")
-                ,((mod4Mask                     , xK_F6), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_pacman.sh")
-                ,((mod4Mask                     , xK_F7), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_date.sh")
-                ,((mod4Mask                     , xK_F8), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_log.sh")
-                ,((0                            , xK_Print), spawn "scrot & mplayer /usr/share/sounds/freedesktop/stereo/screen-capture.oga")
-                ,((mod4Mask                     , xK_Print), spawn "scrot -s & mplayer /usr/share/sounds/freedesktop/stereo/screen-capture.oga")
-                ,((0                            , xF86XK_AudioLowerVolume), spawn "amixer set Master 2- & mplayer /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga")
-                ,((0                            , xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+ & mplayer /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga")
-                ,((0                            , xF86XK_AudioMute), spawn "amixer set Master toggle")
-                ,((0                            , xF86XK_Sleep), spawn "sudo suspend")
-                ,((0                            , xF86XK_AudioPlay), spawn "ncmpcpp toggle")
-                ,((0                            , xF86XK_AudioNext), spawn "ncmpcpp next")
-                ,((0                            , xF86XK_AudioPrev), spawn "ncmpcpp prev")
-                -- Overwrite Xinerama settings for Dvorak Layout
-                -- mod-{w,v,z}, Switch to physical/Xinerama screens 1, 2, or 3
-                -- mod-shift-{w,v,z}, Move client to screen 1, 2, or 3
+                -- Window and Program sittings for Dvorak Layout
+                ,((mod4Mask                     , xK_apostrophe), windows W.focusMaster)
+                ,((mod4Mask                     , xK_f), toggleFloat)
+                ,((mod4Mask                     , xK_n), windows W.focusDown)
+                ,((mod4Mask .|. shiftMask       , xK_n), windows W.swapDown)
+                ,((mod4Mask                     , xK_t), windows W.focusUp)
+                ,((mod4Mask .|. shiftMask       , xK_t), windows W.swapUp)
+                ,((mod4Mask                     , xK_s), sendMessage Shrink)
+                ,((mod4Mask .|. shiftMask       , xK_h), sendMessage MirrorShrink)
+                ,((mod4Mask                     , xK_h), sendMessage Expand)
+                ,((mod4Mask .|. shiftMask       , xK_s), sendMessage MirrorExpand)
+                -- Xinerama settings for Dvorak Layout
                 ,((mod4Mask                     , xK_w), viewScreen 0)
                 ,((mod4Mask                     , xK_v), viewScreen 1)
                 ,((mod4Mask                     , xK_z), viewScreen 2)
@@ -185,6 +161,29 @@ main = do
 --                [((modMask .|. mask, key), f sc)
 --                       | (key, sc) <- zip [xK_w, xK_v, xK_z] [0..]
 --                       , (f, mask) <- [(viewScreen, 0), (sendToScreen, shiftMask)]]
+                -- Dzen scripts
+                ,((mod4Mask                     , xK_F1), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_music.sh")
+                ,((mod4Mask                     , xK_F2), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_vol.sh")
+                ,((mod4Mask                     , xK_F3), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_network.sh")
+                ,((mod4Mask                     , xK_F4), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_battery.sh")
+                ,((mod4Mask                     , xK_F5), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_hardware.sh")
+                ,((mod4Mask                     , xK_F6), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_pacman.sh")
+                ,((mod4Mask                     , xK_F7), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_date.sh")
+                ,((mod4Mask                     , xK_F8), spawn "~/.xmonad/sc ~/.xmonad/scripts/dzen_log.sh")
+                --Special keys
+                ,((0                            , xK_Print), spawn "scrot & mplayer /usr/share/sounds/freedesktop/stereo/screen-capture.oga")
+                ,((mod4Mask                     , xK_Print), spawn "scrot -s & mplayer /usr/share/sounds/freedesktop/stereo/screen-capture.oga")
+                ,((0                            , xF86XK_AudioLowerVolume), spawn "amixer set Master 2- & mplayer /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga")
+                ,((0                            , xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+ & mplayer /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga")
+                ,((0                            , xF86XK_AudioMute), spawn "amixer set Master toggle")
+                ,((0                            , xF86XK_Sleep), spawn "sudo suspend")
+                ,((0                            , xF86XK_AudioPlay), spawn "ncmpcpp toggle")
+                ,((0                            , xF86XK_AudioNext), spawn "ncmpcpp next")
+                ,((0                            , xF86XK_AudioPrev), spawn "ncmpcpp prev")
+                -- Application shortcuts
+                ,((mod4Mask .|. shiftMask       , xK_b), spawn "~/.scripts/wpchanger")
+--                ,((mod4Mask .|. shiftMask       , xK_b), spawn "chromium")
+--                ,((mod4Mask .|. shiftMask       , xK_t), spawn "urxvt -e tmux")
                 ]
                 `additionalMouseBindings`
                 [((mod4Mask                     , 6), (\_ -> moveTo Next NonEmptyWS))
@@ -192,6 +191,13 @@ main = do
                 ,((mod4Mask                     , 5), (\_ -> moveTo Prev NonEmptyWS))
                 ,((mod4Mask                     , 4), (\_ -> moveTo Next NonEmptyWS))
                 ]
+                `removeKeys`
+                [(mod4Mask .|. shiftMask, xK_c)]
+        where toggleFloat = withFocused (\windowId -> do
+                                           { floats <- gets (W.floating . windowset);
+                                             if windowId `M.member` floats
+                                             then withFocused $ windows . W.sink
+                                             else float windowId })
 
 myTerminal      = "urxvt"
 myBitmapsDir    = "~/.xmonad/icons/"
@@ -201,28 +207,20 @@ myFont          = "xft:PragmataPro:style=Regular:pixelsize=14"
 -- Old Colours. (Check their usage and clean them out if not needed)
 background    = "#000000"
 foreground    = "#ffffff"
-
 black0        = "#343638"
 black1        = "#404040"
-
 red0          = "#2f468e"
 red1          = "#7791e0"
-
 green0        = "#424242"
 green1        = "#828a8c"
-
 yellow0       = "#6b8ba3"
 yellow1       = "#8ebdde"
-
 blue0         = "#1c4582"
 blue1         = "#5365a6"
-
 magenta0      = "#74636d"
 magenta1      = "#927d9e"
-
 cyan0         = "#556c85"
 cyan1         = "#6e98b8"
-
 white0        =  "#b2b2b2"
 white1        = "#bdbdbd"
 
@@ -231,8 +229,6 @@ fg_normal     = "#aaaaaa"
 fg_focus      = "#7788af"
 fg_urgent     = "#94738c"
 fg_minimize   = "#444444"
-
-fg_black      = "#424242"
 fg_red        = "#ce5666"
 fg_green      = "#80a673"
 fg_yellow     = "#ffaf5f"
